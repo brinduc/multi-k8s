99166ac0e0870d18d877f7c8db330fc0c72b96af
Laura Squire <brinduc@gmail.com>
2023-01-16 19:14:23 -0800

    Added configuration scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t brinduc/multi-client:latest -t brinduc/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t brinduc/multi-server:latest -t brinduc/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t brinduc/multi-worker:latest -t brinduc/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push brinduc/multi-client:latest
+docker push brinduc/multi-client:$SHA
+
+docker push brinduc/multi-server:latest
+docker push brinduc/multi-server:$SHA
+
+docker push brinduc/multi-worker:latest
+docker push brinduc/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deplopyment server=brinduc/multi-server:$SHA
+kubectl set image deployments/client-deplopyment client=brinduc/multi-client:$SHA
+kubectl set image deployments/worker-deplopyment worker=brinduc/multi-worker:$SHA
